92c27385f212c7e42bd9caed95eb97030e869fd9
Giulio Corallo <s282380@studenti.polito.it>
2022-09-08 23:03:01 +0200

    risultati piu calibrati

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import itertools
 
 
-def subset_sum(weigths, target, partial={}, res=[]):
+def subset_sum(weigths, target, partial=None, res=None):
+    if res is None:
+        res = []
+    if partial is None:
+        partial = {}
     s = sum(partial.values())
 
     # check if the partial sum is equals to target
@@ -19,33 +23,58 @@ def subset_sum(weigths, target, partial={}, res=[]):
         subset_sum(remaining, target, new, res)
 
 
-def get_disjoint_two_set(all_combinations):
+def get_disjoint_two_set(all_combinations_):
     dis_comb = []
-    for i, j in itertools.combinations(all_combinations, 2):
+    for i, j in itertools.combinations(all_combinations_, 2):
         # an empty set is True, a nonempty set is False
         if not set(i).intersection(set(j)):
             dis_comb.append((i, j))
     return dis_comb
 
+def get_eurecom_grades(comb, subject_grade_):
+    first_weighted_sum_ = {k: comb[0].get(k, 0) * subject_grade_.get(k, 0) for k in set(comb[0]) & set(subject_grade_)}
+    second_weighted_sum_ = {k: comb[1].get(k, 0) * subject_grade_.get(k, 0) for k in set(comb[1]) & set(subject_grade_)}
+    first_block_ = sum(first_weighted_sum_.values()) / sum(comb[0].values())
+    second_block_ = sum(second_weighted_sum_.values()) / sum(comb[1].values())
+    return first_block_, second_block_
 
-def get_maximum_score(filtered_comb, subject_grade, num_ects_per_block=25):
-    best = filtered_comb[0]
-    best_score = 0
-    for comb in filtered_comb:
-        first_weighted_sum = {k: comb[0].get(k, 0) * subject_grade.get(k, 0) for k in set(comb[0]) & set(subject_grade)}
-        second_weighted_sum = {k: comb[1].get(k, 0) * subject_grade.get(k, 0) for k in set(comb[1]) & set(subject_grade)}
-        first_block = sum(first_weighted_sum.values()) / sum(comb[0].values())
-        second_block = sum(second_weighted_sum.values()) / sum(comb[1].values())
-        poli_first_block = first_block * 1.8
-        poli_second_block = second_block * 1.8
-        if poli_first_block > 30:
-            poli_first_block = 30
-        if poli_second_block > 30:
-            poli_second_block = 30
-        if round(poli_first_block) + round(poli_second_block) > best_score:
-            best_score = round(poli_first_block) + round(poli_second_block)
-            best = comb
-    return best
+def get_poli_grades(comb, subject_grade_):
+    first_block_, second_block_ = get_eurecom_grades(comb, subject_grade_)
+    poli_first_block = first_block_ * 1.8
+    poli_second_block = second_block_ * 1.8
+    if poli_first_block > 30:
+        poli_first_block = 30
+    if poli_second_block > 30:
+        poli_second_block = 30
+    return poli_first_block, poli_second_block
+
+def get_maximum_score(filtered_comb_, subject_grade_):
+    best_first = 0
+    best_second = 0
+    best_ = filtered_comb_[0]
+    list_best = []
+    min_diff = 30
+    for comb in filtered_comb_:
+        poli_first_block, poli_second_block = get_poli_grades(comb, subject_grade_)
+        if round(poli_first_block) + round(poli_second_block) > best_first + best_second:
+            best_first = round(poli_first_block)
+            best_second = round(poli_second_block)
+            best_ = comb
+
+    # second round
+    for comb in filtered_comb_:
+        poli_first_block, poli_second_block = get_poli_grades(comb, subject_grade_)
+        if round(poli_first_block) == best_first and round(poli_second_block) == best_second:
+            list_best.append(comb)
+
+    # balance
+    for comb in list_best:
+        first_block_, second_block_ = get_eurecom_grades(comb, subject_grade_)
+        if (second_block_ - first_block_) < min_diff:
+            best_ = comb
+            min_diff = second_block_ - first_block_
+
+    return best_
 
 
 if __name__ == '__main__':
